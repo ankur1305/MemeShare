96297d45bd5bfea8af7587d6ebc58c725108d011
ankur1305 <99ankursinghal@gmail.com>
2020-12-01 17:57:17 +0530

    Some minor changes

--- app/src/main/java/com/singhalankur/memeshare/MainActivity.java
+++ app/src/main/java/com/singhalankur/memeshare/MainActivity.java
@@ -7,14 +7,18 @@ import android.content.Intent;
 import android.graphics.drawable.Drawable;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.Toast;
+
 import com.android.volley.Request;
+import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.DataSource;
 import com.bumptech.glide.load.engine.GlideException;
@@ -39,21 +43,45 @@ public class MainActivity extends AppCompatActivity {
 
         memeIv = findViewById(R.id.memeIv);
         progressBar = findViewById(R.id.progressBar);
+        ImageView nextBtn = findViewById(R.id.nextBtn);
+        ImageView shareBtn = findViewById(R.id.shareBtn);
+
+        nextBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                fetchData();
+            }
+        });
+
+        shareBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent sendIntent = new Intent();
+                sendIntent.setType("text/plain");
+                sendIntent.setAction(Intent.ACTION_SEND);
+                sendIntent.putExtra(Intent.EXTRA_TEXT, "Hey, check out this meme on : " + currentImageUrl);
+
+                Intent shareIntent = Intent.createChooser(sendIntent, "Share this link");
+                startActivity(shareIntent);
+            }
+        });
 
         fetchData();
     }
 
     private void fetchData() {
         progressBar.setVisibility(View.VISIBLE);
+        RequestQueue queue = Volley.newRequestQueue(this);
         String url = "https://meme-api.herokuapp.com/gimme";
 
-        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
-                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
-                    @Override
-                    public void onResponse(JSONObject response) {
-                        try {
-                            currentImageUrl = response.getString("url");
-                            Glide.with(MainActivity.this).load(currentImageUrl).listener(new RequestListener<Drawable>() {
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    currentImageUrl = response.getString("url");
+                    Glide.with(MainActivity.this).load(currentImageUrl).listener(
+                            new RequestListener<Drawable>() {
                                 @Override
                                 public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
                                     progressBar.setVisibility(View.GONE);
@@ -65,32 +93,19 @@ public class MainActivity extends AppCompatActivity {
                                     progressBar.setVisibility(View.GONE);
                                     return false;
                                 }
-                            }).into(memeIv);
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                }, new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        Toast.makeText(MainActivity.this, "Something Went Wrong", Toast.LENGTH_SHORT).show();
-                    }
-                });
-        MySingleton.getInstance(this).addToRequestQueue(jsonObjectRequest);
+                            }
+                    ).into(memeIv);
+                } catch (JSONException e) {
+                    Log.d("Exception:", e.toString());
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(MainActivity.this, "Something went wrong."+error.toString(), Toast.LENGTH_SHORT).show();
+            }
+        }
+        );
+        queue.add(jsonObjectRequest);
     }
-
-    public void nextMeme(View view) {
-        fetchData();
-    }
-
-    public void shareMeme(View view) {
-        Intent sendIntent = new Intent();
-        sendIntent.setAction(Intent.ACTION_SEND);
-        sendIntent.putExtra(Intent.EXTRA_TEXT, "Hey, Check out this amazing meme on  "+currentImageUrl);
-        sendIntent.setType("text/plain");
-
-        Intent shareIntent = Intent.createChooser(sendIntent, "Share this link by...");
-        startActivity(shareIntent);
-    }
-
 }
